817a3482e450a033d0ebd8279c88bb2837809a0e
Ernie Nettles <ernienettles@gmail.com>
2020-04-08 01:39:12 -0400

    Finished

--- js/index.js
+++ js/index.js
@@ -8,29 +8,31 @@ const get = (selector) => {
 
 const bus = get('.bus')
 const textContent = get('.text-content')
-const go = get('#cancelButton')
-const successMessage = get('.report.success')
-const failureMessage = get('.report.failure')
-const modal = get('.modal')
-const modalOpacity = get('.modal-opacity')
+const nav = get('.main-navigation')
+const content = get('.content-section')
+const logo = get('.container .logo-heading') 
 
 function driveTheBus(event) {
-    bus.classList.add('go')
+    if(event.key === "Escape"){
+        bus.classList.add('go')
+    }
   }
-bus.addEventListener('click', driveTheBus)
+document.addEventListener('keydown', driveTheBus)
 
 function resetTheBus(event) {
     bus.classList.remove('go')
   }
-bus.addEventListener('dblclick', resetTheBus)
+document.addEventListener('keyup', resetTheBus)
 
 function drivingTheBus(event) {
-    alert('You started the bus!')
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  nav.style.backgroundColor = '#' + randomColor;
   }
 bus.addEventListener('animationstart', drivingTheBus)
 
 function stopTheBus(event) {
-    alert("The bus completed it's trip!")
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.color = '#' + randomColor;
   }
 bus.addEventListener('animationend', stopTheBus)
 
@@ -38,6 +40,45 @@ function againTheBus(event) {
     alert("You copied the text!")
   }
 textContent.addEventListener('copy', againTheBus)
+
+function randomBG(event) {
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = '#' + randomColor;
+}
+document.addEventListener('wheel', randomBG)
+
+function resetBG(event) {
+  document.body.style.backgroundColor = 'white';
+}
+document.addEventListener('scroll', resetBG)
+
+function makePBig(event) {
+  textContent.classList.add('big');
+}
+document.addEventListener('dblclick', makePBig)
+
+function makePSmall(event) {
+  textContent.classList.remove('big');
+}
+document.addEventListener('auxclick', makePSmall)
+
+function logoBG(event) {
+  logo.style.backgroundColor = 'red';
+  event.preventDefault();
+}
+logo.addEventListener('auxclick', logoBG)
+
+function navBG(event) {
+  nav.style.backgroundColor = 'blue';
+  event.preventDefault();
+}
+nav.addEventListener('click', navBG)
+
+
+function nope(event) {
+  alert("Nope. Leave.");
+}
+window.addEventListener('load', nope)
 
 
 
